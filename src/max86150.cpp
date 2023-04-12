e0e7deb592534a0f7a50cfcb6ffc57b79f779594
Ryo Hajika <ryo@imaginaryshort.com>
2023-04-12 15:38:11 +1200

    initial update - register addresses edit and added "ppgSetup" function to get PPG functionalities working okay first thing first

--- src/max86150.cpp
+++ src/max86150.cpp
@@ -9,115 +9,6 @@
 
 #include "max86150.h"
 
-static const uint8_t MAX86150_INTSTAT1 =		0x00;
-static const uint8_t MAX86150_INTSTAT2 =		0x01;
-static const uint8_t MAX86150_INTENABLE1 =		0x02;
-static const uint8_t MAX86150_INTENABLE2 =		0x03;
-
-static const uint8_t MAX86150_FIFOWRITEPTR = 	0x04;
-static const uint8_t MAX86150_FIFOOVERFLOW = 	0x05;
-static const uint8_t MAX86150_FIFOREADPTR = 	0x06;
-static const uint8_t MAX86150_FIFODATA 		=		0x07;
-
-static const uint8_t MAX86150_FIFOCONFIG = 		0x08;
-static const uint8_t MAX86150_FIFOCONTROL1= 	0x09;
-static const uint8_t MAX86150_FIFOCONTROL2 = 	0x0A;
-
-static const uint8_t MAX86150_SYSCONTROL = 	0x0D;
-static const uint8_t MAX86150_PPGCONFIG1 = 		0x0E;
-static const uint8_t MAX86150_PPGCONFIG2 = 		0x0F;
-static const uint8_t MAX86150_LED_PROX_AMP = 	0x10;
-
-static const uint8_t MAX86150_LED1_PULSEAMP = 	0x11;
-static const uint8_t MAX86150_LED2_PULSEAMP = 	0x12;
-static const uint8_t MAX86150_LED_RANGE 		= 	0x14;
-static const uint8_t MAX86150_LED_PILOT_PA 	= 	0x15;
-
-static const uint8_t MAX86150_ECG_CONFIG1 	= 	0x3C;
-static const uint8_t MAX86150_ECG_CONFIG3 	= 	0x3E;
-static const uint8_t MAX86150_PROXINTTHRESH = 	0x10;
-
-static const uint8_t MAX86150_PARTID = 			0xFF;
-
-// MAX86150 Commands
-static const uint8_t MAX86150_INT_A_FULL_MASK =		(byte)~0b10000000;
-static const uint8_t MAX86150_INT_A_FULL_ENABLE = 	0x80;
-static const uint8_t MAX86150_INT_A_FULL_DISABLE = 	0x00;
-
-static const uint8_t MAX86150_INT_DATA_RDY_MASK = (byte)~0b01000000;
-static const uint8_t MAX86150_INT_DATA_RDY_ENABLE =	0x40;
-static const uint8_t MAX86150_INT_DATA_RDY_DISABLE = 0x00;
-
-static const uint8_t MAX86150_INT_ALC_OVF_MASK = (byte)~0b00100000;
-static const uint8_t MAX86150_INT_ALC_OVF_ENABLE = 	0x20;
-static const uint8_t MAX86150_INT_ALC_OVF_DISABLE = 0x00;
-
-static const uint8_t MAX86150_INT_PROX_INT_MASK = (byte)~0b00010000;
-static const uint8_t MAX86150_INT_PROX_INT_ENABLE = 0x10;
-static const uint8_t MAX86150_INT_PROX_INT_DISABLE = 0x00;
-
-static const uint8_t MAX86150_SAMPLEAVG_MASK =	(byte)~0b11100000;
-static const uint8_t MAX86150_SAMPLEAVG_1 = 	0x00;
-static const uint8_t MAX86150_SAMPLEAVG_2 = 	0x20;
-static const uint8_t MAX86150_SAMPLEAVG_4 = 	0x40;
-static const uint8_t MAX86150_SAMPLEAVG_8 = 	0x60;
-static const uint8_t MAX86150_SAMPLEAVG_16 = 	0x80;
-static const uint8_t MAX86150_SAMPLEAVG_32 = 	0xA0;
-
-static const uint8_t MAX86150_ROLLOVER_MASK = 	0xEF;
-static const uint8_t MAX86150_ROLLOVER_ENABLE = 0x10;
-static const uint8_t MAX86150_ROLLOVER_DISABLE = 0x00;
-
-static const uint8_t MAX86150_A_FULL_MASK = 	0xF0;
-
-static const uint8_t MAX86150_SHUTDOWN_MASK = 	0x7F;
-static const uint8_t MAX86150_SHUTDOWN = 		0x80;
-static const uint8_t MAX86150_WAKEUP = 			0x00;
-
-static const uint8_t MAX86150_RESET_MASK = 		0xFE;
-static const uint8_t MAX86150_RESET = 			0x01;
-
-static const uint8_t MAX86150_MODE_MASK = 		0xF8;
-static const uint8_t MAX86150_MODE_REDONLY = 	0x02;
-static const uint8_t MAX86150_MODE_REDIRONLY = 	0x03;
-static const uint8_t MAX86150_MODE_MULTILED = 	0x07;
-
-static const uint8_t MAX86150_ADCRANGE_MASK = 	0x9F;
-static const uint8_t MAX86150_ADCRANGE_2048 = 	0x00;
-static const uint8_t MAX86150_ADCRANGE_4096 = 	0x20;
-static const uint8_t MAX86150_ADCRANGE_8192 = 	0x40;
-static const uint8_t MAX86150_ADCRANGE_16384 = 	0x60;
-
-static const uint8_t MAX86150_SAMPLERATE_MASK = 0xE3;
-static const uint8_t MAX86150_SAMPLERATE_50 = 	0x00;
-static const uint8_t MAX86150_SAMPLERATE_100 = 	0x04;
-static const uint8_t MAX86150_SAMPLERATE_200 = 	0x08;
-static const uint8_t MAX86150_SAMPLERATE_400 = 	0x0C;
-static const uint8_t MAX86150_SAMPLERATE_800 = 	0x10;
-static const uint8_t MAX86150_SAMPLERATE_1000 = 0x14;
-static const uint8_t MAX86150_SAMPLERATE_1600 = 0x18;
-static const uint8_t MAX86150_SAMPLERATE_3200 = 0x1C;
-
-static const uint8_t MAX86150_PULSEWIDTH_MASK = 0xFC;
-static const uint8_t MAX86150_PULSEWIDTH_69 = 	0x00;
-static const uint8_t MAX86150_PULSEWIDTH_118 = 	0x01;
-static const uint8_t MAX86150_PULSEWIDTH_215 = 	0x02;
-static const uint8_t MAX86150_PULSEWIDTH_411 = 	0x03;
-
-static const uint8_t MAX86150_SLOT1_MASK = 		0xF0;
-static const uint8_t MAX86150_SLOT2_MASK = 		0x0F;
-static const uint8_t MAX86150_SLOT3_MASK = 		0xF0;
-static const uint8_t MAX86150_SLOT4_MASK = 		0x0F;
-
-static const uint8_t SLOT_NONE 				= 			0x00;
-static const uint8_t SLOT_RED_LED 		= 			0x01;
-static const uint8_t SLOT_IR_LED 			= 			0x02;
-static const uint8_t SLOT_RED_PILOT 	=				0x09;
-static const uint8_t SLOT_IR_PILOT 		= 			0x0A;
-static const uint8_t SLOT_ECG					= 			0x0D;
-
-static const uint8_t MAX_30105_EXPECTEDPARTID = 0x1E;
-
 MAX86150::MAX86150() {
   // Constructor
 }
@@ -212,22 +103,22 @@ void MAX86150::wakeUp(void) {
 void MAX86150::setLEDMode(uint8_t mode) {
   // Set which LEDs are used for sampling -- Red only, RED+IR only, or custom.
   // See datasheet, page 19
-  //bitMask(MAX86150_PPGCONFIG1, MAX86150_MODE_MASK, mode);
+  bitMask(MAX86150_PPGCONFIG1, MAX86150_MODE_MASK, mode);
 }
 
 void MAX86150::setADCRange(uint8_t adcRange) {
-  // adcRange: one of MAX86150_ADCRANGE_2048, _4096, _8192, _16384
-  //bitMask(MAX86150_PARTICLECONFIG, MAX86150_ADCRANGE_MASK, adcRange);
+  // adcRange: one of MAX86150_ADCRANGE_4096, _8192, _16384, _32768
+  bitMask(MAX86150_PPGCONFIG1, MAX86150_ADCRANGE_MASK, adcRange);
 }
 
 void MAX86150::setSampleRate(uint8_t sampleRate) {
   // sampleRate: one of MAX86150_SAMPLERATE_50, _100, _200, _400, _800, _1000, _1600, _3200
-  //bitMask(MAX86150_PARTICLECONFIG, MAX86150_SAMPLERATE_MASK, sampleRate);
+  bitMask(MAX86150_PPGCONFIG1, MAX86150_SAMPLERATE_MASK, sampleRate);
 }
 
 void MAX86150::setPulseWidth(uint8_t pulseWidth) {
   // pulseWidth: one of MAX86150_PULSEWIDTH_69, _188, _215, _411
-  //bitMask(MAX86150_PPGCONFIG1, MAX86150_PULSEWIDTH_MASK, pulseWidth);
+  bitMask(MAX86150_PPGCONFIG1, MAX86150_PULSEWIDTH_MASK, pulseWidth);
 }
 
 // NOTE: Amplitude values: 0x00 = 0mA, 0x7F = 25.4mA, 0xFF = 50mA (typical)
@@ -243,7 +134,7 @@ void MAX86150::setPulseAmplitudeIR(uint8_t amplitude)
 }
 
 void MAX86150::setPulseAmplitudeProximity(uint8_t amplitude) {
-  writeRegister8(_i2caddr, MAX86150_LED_PROX_AMP, amplitude);
+  writeRegister8(_i2caddr, MAX86150_LED_PILOT_PA, amplitude);
 }
 
 void MAX86150::setProximityThreshold(uint8_t threshMSB)
@@ -410,6 +301,86 @@ void MAX86150::setup(byte powerLevel, byte sampleAverage, byte ledMode, int samp
   	clearFIFO(); //Reset the FIFO before we begin checking the sensor
 }
 
+void MAX86150::ppgSetup(bool enRed, bool enIR,
+                        uint8_t redCurrentRange, uint8_t irCurrentRange,
+                        uint8_t redPower, uint8_t irPower, uint8_t sampleAverage,
+                        uint8_t sampleRate, uint8_t pulseWidth, uint8_t adcRange){
+
+    writeRegister8(_i2caddr,MAX86150_SYSCONTROL,0x01);
+    delay(100);
+    writeRegister8(_i2caddr,MAX86150_FIFOCONFIG,0x7F);
+
+    //FIFO Configuration
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    //The chip will average multiple samples of same type together if you wish
+    setFIFOAverage(sampleAverage);
+    setADCRange(adcRange);
+    setSampleRate(sampleRate);
+    setPulseWidth(pulseWidth);
+
+    uint16_t FIFOCode = 0x00;
+
+    FIFOCode = FIFOCode<<4 | 0x0009;// : FIFOCode;  //insert ECG front of ETI in FIFO
+    FIFOCode = FIFOCode<<8 | 0x0021;//) : FIFOCode; //insert Red(2) and IR (1) in front of ECG in FIFO
+
+    // LED1: IR, LED2: RED
+    //FIFO Control 1 = FD2|FD1, FIFO Control 2 = FD4|FD3
+    if(enRed == true && enIR == false){
+        activeDevices = 1;
+        writeRegister8(_i2caddr,MAX86150_FIFOCONTROL1,(0b00100000));
+        writeRegister8(_i2caddr,MAX86150_FIFOCONTROL2,(0b00000000));
+        writeRegister8(_i2caddr,MAX86150_SYSCONTROL,0x04);//start FIFO
+        bitMask(MAX86150_LED_RANGE, MAX86150_LED2_RGE_MASK, redCurrentRange);
+        setPulseAmplitudeRed(redPower);
+    }
+    else if(enRed == false && enIR == true){
+        activeDevices = 1;
+        writeRegister8(_i2caddr,MAX86150_FIFOCONTROL1,(0b00000001));
+        writeRegister8(_i2caddr,MAX86150_FIFOCONTROL2,(0b00000000));
+        writeRegister8(_i2caddr,MAX86150_SYSCONTROL,0x04);//start FIFO
+        bitMask(MAX86150_LED_RANGE, MAX86150_LED1_RGE_MASK, irCurrentRange);
+        setPulseAmplitudeIR(irPower);
+    }
+    else if(enRed == true && enIR == true){
+        activeDevices = 2;
+        writeRegister8(_i2caddr,MAX86150_FIFOCONTROL1,(0b00100001));
+        writeRegister8(_i2caddr,MAX86150_FIFOCONTROL2,(0b00000000));
+        writeRegister8(_i2caddr,MAX86150_SYSCONTROL,0x04);//start FIFO
+        bitMask(MAX86150_LED_RANGE, MAX86150_LED2_RGE_MASK, redCurrentRange);
+        bitMask(MAX86150_LED_RANGE, MAX86150_LED1_RGE_MASK, irCurrentRange);
+        setPulseAmplitudeRed(redPower);
+        setPulseAmplitudeIR(irPower);
+    }
+    else{   // LEDs + ECG
+        activeDevices = 3;
+        writeRegister8(_i2caddr,MAX86150_FIFOCONTROL1,(0b00100001));
+        writeRegister8(_i2caddr,MAX86150_FIFOCONTROL2,(0b00001001));
+
+        writeRegister8(_i2caddr,MAX86150_SYSCONTROL,0x04);//start FIFO
+
+        writeRegister8(_i2caddr,MAX86150_ECG_CONFIG1,0b00000011);///0b00000011); // SR: 200
+        writeRegister8(_i2caddr,MAX86150_ECG_CONFIG3,0b00001101); // IA Gain: 9.5 / PGA Gain: 8
+
+        setPulseAmplitudeRed(redPower);
+        setPulseAmplitudeIR(irPower);
+    }
+    
+
+//Multi-LED Mode Configuration, Enable the reading of the three LEDs
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//enableSlot(1, SLOT_RED_LED);
+//if (ledMode > 1)
+//enableSlot(2, SLOT_IR_LED);
+//if (ledMode > 2)
+//enableSlot(3, SLOT_ECG);
+//enableSlot(1, SLOT_RED_PILOT);
+//enableSlot(2, SLOT_IR_PILOT);
+//enableSlot(3, SLOT_GREEN_PILOT);
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+  clearFIFO(); //Reset the FIFO before we begin checking the sensor
+}
+
 //Tell caller how many samples are available
 uint8_t MAX86150::available(void)
 {
@@ -423,7 +394,7 @@ uint8_t MAX86150::available(void)
 uint32_t MAX86150::getRed(void)
 {
   //Check the sensor for new data for 250ms
-  if(safeCheck(250))
+  if(safeCheck(200))
     return (sense.red[sense.head]);
   else
     return(0); //Sensor failed to find new data
@@ -433,7 +404,7 @@ uint32_t MAX86150::getRed(void)
 uint32_t MAX86150::getIR(void)
 {
   //Check the sensor for new data for 250ms
-  if(safeCheck(250))
+  if(safeCheck(200))
     return (sense.IR[sense.head]);
   else
     return(0); //Sensor failed to find new data
@@ -443,7 +414,7 @@ uint32_t MAX86150::getIR(void)
 int32_t MAX86150::getECG(void)
 {
   //Check the sensor for new data for 250ms
-  if(safeCheck(250))
+  if(safeCheck(200))
     return (sense.ecg[sense.head]);
   else
     return(0); //Sensor failed to find new data
--- src/max86150.h
+++ src/max86150.h
@@ -43,6 +43,142 @@
 
 #endif
 
+static const uint8_t MAX86150_INTSTAT1 =        0x00;
+static const uint8_t MAX86150_INTSTAT2 =        0x01;
+static const uint8_t MAX86150_INTENABLE1 =        0x02;
+static const uint8_t MAX86150_INTENABLE2 =        0x03;
+
+static const uint8_t MAX86150_FIFOWRITEPTR =     0x04;
+static const uint8_t MAX86150_FIFOOVERFLOW =     0x05;
+static const uint8_t MAX86150_FIFOREADPTR =     0x06;
+static const uint8_t MAX86150_FIFODATA =        0x07;
+
+static const uint8_t MAX86150_FIFOCONFIG =         0x08;
+static const uint8_t MAX86150_FIFOCONTROL1 =    0x09;
+static const uint8_t MAX86150_FIFOCONTROL2 =     0x0A;
+
+static const uint8_t MAX86150_SYSCONTROL =      0x0D;
+static const uint8_t MAX86150_PPGCONFIG1 =         0x0E;
+static const uint8_t MAX86150_PPGCONFIG2 =         0x0F;
+static const uint8_t MAX86150_PROXINTTHRESH =   0x10;
+
+static const uint8_t MAX86150_LED1_PULSEAMP =     0x11;   // IR
+static const uint8_t MAX86150_LED2_PULSEAMP =     0x12;   // Red
+static const uint8_t MAX86150_LED_RANGE =       0x14;
+static const uint8_t MAX86150_LED_PILOT_PA     =     0x15;
+
+static const uint8_t MAX86150_ECG_CONFIG1     =     0x3C;
+static const uint8_t MAX86150_ECG_CONFIG3     =     0x3E;
+
+static const uint8_t MAX86150_PARTID =             0xFF;
+
+// MAX86150 Commands
+// FIFO Almost Full Flag (INT1)
+static const uint8_t MAX86150_INT_A_FULL_MASK =         (byte)~0b10000000;
+static const uint8_t MAX86150_INT_A_FULL_ENABLE =         0x80;
+static const uint8_t MAX86150_INT_A_FULL_DISABLE =         0x00;
+
+// new PPG FIFO data ready (INT1)
+static const uint8_t MAX86150_INT_DATA_RDY_MASK =       (byte)~0b01000000;
+static const uint8_t MAX86150_INT_DATA_RDY_ENABLE =     0x40;
+static const uint8_t MAX86150_INT_DATA_RDY_DISABLE =    0x00;
+
+// Ambient Light Cancellation Overflow (INT1)
+static const uint8_t MAX86150_INT_ALC_OVF_MASK =        (byte)~0b00100000;
+static const uint8_t MAX86150_INT_ALC_OVF_ENABLE =         0x20;
+static const uint8_t MAX86150_INT_ALC_OVF_DISABLE =     0x00;
+
+// Proximity Interrupt (INT1)
+static const uint8_t MAX86150_INT_PROX_INT_MASK =       (byte)~0b00010000;
+static const uint8_t MAX86150_INT_PROX_INT_ENABLE =     0x10;
+static const uint8_t MAX86150_INT_PROX_INT_DISABLE =    0x00;
+
+// Power Ready Flag (INT1)
+static const uint8_t MAX86150_INT_PWR_RDY_MASK =        (byte)~0b00000001;
+static const uint8_t MAX86150_INT_PWR_RDY_ENABLE =      0x01;
+static const uint8_t MAX86150_INT_PWR_RDY_DISABLE =     0x00;
+
+// New ECG FIFO data ready (INT2)
+static const uint8_t MAX86150_INT_ECG_RDY_MASK =        (byte)~0b00000100;
+static const uint8_t MAX86150_INT_ECG_RDY_ENABLE =      0x04;
+static const uint8_t MAX86150_INT_ECG_RDY_DISABLE =     0x00;
+
+// INT EN 1 0x02
+// [7: A_FULL_EN][6: PPG_RDY_EN][5: ALC_OVF_ON][4: PROX_INT_EN]...
+
+// INT EN 2 0x03
+// [7: VDD_OOR_EN]...[2: ECG_RDY_EN]...
+
+static const uint8_t MAX86150_SAMPLEAVG_MASK =  (byte)~0b00000111;
+static const uint8_t MAX86150_SAMPLEAVG_1 =     0x00;
+static const uint8_t MAX86150_SAMPLEAVG_2 =     0x01;
+static const uint8_t MAX86150_SAMPLEAVG_4 =     0x02;
+static const uint8_t MAX86150_SAMPLEAVG_8 =     0x03;
+static const uint8_t MAX86150_SAMPLEAVG_16 =     0x04;
+static const uint8_t MAX86150_SAMPLEAVG_32 =     0x05;
+
+static const uint8_t MAX86150_ROLLOVER_MASK =     0xEF;
+static const uint8_t MAX86150_ROLLOVER_ENABLE = 0x10;
+static const uint8_t MAX86150_ROLLOVER_DISABLE = 0x00;
+
+static const uint8_t MAX86150_A_FULL_MASK =     0xF0;
+
+static const uint8_t MAX86150_SHUTDOWN_MASK =     (byte)~0b00000010;
+static const uint8_t MAX86150_SHUTDOWN =         0x02;
+static const uint8_t MAX86150_WAKEUP =             0x00;
+
+static const uint8_t MAX86150_RESET_MASK =         (byte)~0b00000001;
+static const uint8_t MAX86150_RESET =             0x01;
+
+static const uint8_t MAX86150_MODE_MASK =         0xF8;
+static const uint8_t MAX86150_MODE_REDONLY =     0x02;
+static const uint8_t MAX86150_MODE_REDIRONLY =     0x03;
+static const uint8_t MAX86150_MODE_MULTILED =     0x07;
+
+static const uint8_t MAX86150_ADCRANGE_MASK =     (byte)~0b11000000;
+static const uint8_t MAX86150_ADCRANGE_4096 =     0x00;
+static const uint8_t MAX86150_ADCRANGE_8192 =     0x40;
+static const uint8_t MAX86150_ADCRANGE_16384 =     0x80;
+static const uint8_t MAX86150_ADCRANGE_32768 =     0xC0;
+
+static const uint8_t MAX86150_SAMPLERATE_MASK = (byte)~0b00111100;
+static const uint8_t MAX86150_SAMPLERATE_50 =     0x04;
+static const uint8_t MAX86150_SAMPLERATE_100 =     0x10;
+static const uint8_t MAX86150_SAMPLERATE_200 =     0x14;
+static const uint8_t MAX86150_SAMPLERATE_400 =     0x18;
+static const uint8_t MAX86150_SAMPLERATE_800 =     0x1C;
+static const uint8_t MAX86150_SAMPLERATE_1000 = 0x2C;
+static const uint8_t MAX86150_SAMPLERATE_1600 = 0x24;
+static const uint8_t MAX86150_SAMPLERATE_3200 = 0x28;
+
+static const uint8_t MAX86150_PULSEWIDTH_MASK = (byte)~0b00000011;
+static const uint8_t MAX86150_PULSEWIDTH_50 =     0x00;
+static const uint8_t MAX86150_PULSEWIDTH_100 =     0x01;
+static const uint8_t MAX86150_PULSEWIDTH_200 =     0x02;
+static const uint8_t MAX86150_PULSEWIDTH_400 =     0x03;
+
+static const uint8_t MAX86150_LED1_RGE_MASK =   (byte)~0b00000011;
+static const uint8_t MAX86150_LED1_RGE_50 =     0x00;
+static const uint8_t MAX86150_LED1_RGE_100 =    0x01;
+static const uint8_t MAX86150_LED2_RGE_MASK =   (byte)~0b00001100;
+static const uint8_t MAX86150_LED2_RGE_50 =     0x00;
+static const uint8_t MAX86150_LED2_RGE_100 =    0x04;
+
+static const uint8_t MAX86150_SLOT1_MASK =         0xF0;
+static const uint8_t MAX86150_SLOT2_MASK =         0x0F;
+static const uint8_t MAX86150_SLOT3_MASK =         0xF0;
+static const uint8_t MAX86150_SLOT4_MASK =         0x0F;
+
+static const uint8_t SLOT_NONE =                0x00;
+static const uint8_t SLOT_RED_LED =             0x01;
+static const uint8_t SLOT_IR_LED =              0x02;
+static const uint8_t SLOT_RED_PILOT =           0x09;
+static const uint8_t SLOT_IR_PILOT =            0x0A;
+static const uint8_t SLOT_ECG =                 0x0D;
+
+static const uint8_t MAX_30105_EXPECTEDPARTID = 0x1E;
+
+
 class MAX86150 {
  public:
   MAX86150(void);
@@ -123,6 +259,10 @@ class MAX86150 {
 
   // Setup the IC with user selectable settings
   void setup(byte powerLevel = 0x1F, byte sampleAverage = 4, byte ledMode = 3, int sampleRate = 400, int pulseWidth = 411, int adcRange = 4096);
+  void ppgSetup(bool enRed, bool enIR,
+                uint8_t redCurrentRange, uint8_t irCurrentRange,
+                uint8_t redPower, uint8_t irPower, uint8_t sampleAverage,
+                uint8_t sampleRate, uint8_t pulseWidth, uint8_t adcRange);
 
   // Low-level I2C communication
   uint8_t readRegister8(uint8_t address, uint8_t reg);
